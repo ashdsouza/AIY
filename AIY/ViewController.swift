89766308659ba765871d15a733e0304712af4036
ashley_f_dsouza <ashley.f.dsouza@oracle.com>
2018-09-04 12:05:19 -0700

    Initial Commit

---
+++ AIY/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AIY
+//
+//  Created by Ashley F Dsouza on 9/4/18.
+//  Copyright © 2018 Ashley F Dsouza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
