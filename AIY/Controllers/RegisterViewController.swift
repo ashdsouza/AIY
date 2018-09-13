9d8175096462d6ef49973449190b7ac369f46369
ashley_f_dsouza <ashley.f.dsouza@oracle.com>
2018-09-13 12:11:50 -0700

    Added seller information in Users DB, fetch user info for welcome page and image resizing for profile pic

--- AIY/Controllers/RegisterViewController.swift
+++ AIY/Controllers/RegisterViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import os.log
 import CoreData
 
-class RegisterViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class RegisterViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
     
     //MARK: Properties
     @IBOutlet weak var name: UITextField!
@@ -19,16 +19,70 @@ class RegisterViewController: UIViewController, UITextFieldDelegate, UIImagePick
     @IBOutlet weak var photo: UIImageView!
     @IBOutlet weak var registerError: UILabel!
     @IBOutlet weak var register: UIButton!
+    @IBOutlet weak var buyerSellerSwitch: UISwitch!
+    @IBOutlet weak var category: UIPickerView!
+    
+    var categories = [String]()
+    var userType: Bool!
+    var categoryNameSelected: String! = nil
+    let imagePicker = UIImagePickerController()
+    
+//    var appDelegate: AppDelegate!
+//    var context: NSManagedObjectContext!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         registerError.isHidden = true;
+        buyerSellerSwitch.isOn = false;
+        toggleSwitch()
+        
+//        appDelegate = UIApplication.shared.delegate as! AppDelegate;
+//        context = appDelegate.persistentContainer.viewContext
+        
+        //triggers the sellerSelected function when the switch is toggeled
+        buyerSellerSwitch.addTarget(self, action: #selector(sellerSelected(_:)), for: UIControlEvents.valueChanged)
         
         name.delegate = self;
         loginUsername.delegate = self;
         loginPassword.delegate = self;
+        category.delegate = self;
+        category.dataSource = self;
+        imagePicker.delegate = self;
+        
+        //TODO: Fill Data in Category Table so it can show up here
+//        let appDelegate = UIApplication.shared.delegate as! AppDelegate;
+//        let context = appDelegate.persistentContainer.viewContext
+//        
+//        let cat = Category(context: context);
+//        cat.name = "Items";
+//        
+//        appDelegate.saveContext()
+//        
+//        let cat2 = Category(context: context);
+//        cat2.name = "Cars";
+//        
+//        appDelegate.saveContext()
+//        
+//        let cat3 = Category(context: context);
+//        cat3.name = "Houses";
+//        
+//        appDelegate.saveContext()
+        
+        
+        let result = getDataFromDB("Category")
+        for data in result as! [NSManagedObject] {
+            print(data.value(forKey: "name") as! String)
+            categories.append(data.value(forKey: "name") as! String)
+        }
+//        categories.append("Items")
+//        categories.append("Car")
+//        categories.append("Houses")
+        
+        //TODO: Unless picker is toggeled, category is not selected. Fix this.
+        //set default as "Cars" for now
+        category.selectRow(1, inComponent: 0, animated: false)
     }
 
     override func didReceiveMemoryWarning() {
@@ -47,7 +101,41 @@ class RegisterViewController: UIViewController, UITextFieldDelegate, UIImagePick
         super.prepare(for: segue, sender: sender);
     }
     
+    func toggleSwitch() {
+        if buyerSellerSwitch.isOn {
+            print("Seller Selected display category picker");
+            category.isHidden = false;
+            userType = true;
+        } else {
+            print("It is a Buyer. Hide category picker");
+            category.isHidden = true;
+            userType = false;
+        }
+    }
+    
+    func getDataFromDB(_ entityName: String) -> [Any] {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate;
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: entityName);
+        request.returnsObjectsAsFaults = false
+        do {
+            print("Fetching from DB")
+            let result = try context.fetch(request)
+            return result
+        } catch {
+            print("Failed to fetch")
+            let nserror = error as NSError
+            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+        }
+    }
+    
     //MARK: Actions
+    //display the category of seller selection if it is seller
+    @IBAction func sellerSelected(_ sender: UISwitch) {
+        toggleSwitch()
+    }
+    
     //Lets user pick an image from the photo library
     @IBAction func selectPhotoFromLibrary(_ sender: Any) {
         //Hide keyboard if image is tapped
@@ -62,6 +150,24 @@ class RegisterViewController: UIViewController, UITextFieldDelegate, UIImagePick
         present(photoPickerController, animated: true, completion: nil);
     }
     
+    //MARK: Functions for Category Picker View
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return categories.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return categories[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        categoryNameSelected = categories[row] as String
+    }
+    
+    //MARK: Functions for Image Picker
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         //Dismiss the picker if the user canceled.
         dismiss(animated: true, completion: nil);
@@ -87,19 +193,35 @@ class RegisterViewController: UIViewController, UITextFieldDelegate, UIImagePick
         self.present(loginController, animated: true, completion: nil)
     }
     
-    func storeUserData(_ name: String, _ loginUsername: String, _ loginPassword: String, _ photo: Data) {
+    func storeUserData(_ photo: Data) {
+        
         let appDelegate = UIApplication.shared.delegate as! AppDelegate;
         let context = appDelegate.persistentContainer.viewContext
         
         let newUser = Users(context: context);
-        newUser.name = name;
-        newUser.loginUsername = loginUsername;
-        newUser.loginPassword = loginPassword;
+        newUser.name = self.name.text;
+        newUser.loginUsername = self.loginUsername.text;
+        newUser.loginPassword = self.loginPassword.text;
         newUser.photo = photo as NSData?;
+        newUser.type = userType;
+        newUser.category = categoryNameSelected;
         
         appDelegate.saveContext();
         
         //retrieve data
+//        let result = getDataFromDB("Users")
+//        for data in result as! [NSManagedObject] {
+//            print(data.value(forKey: "name") as! String)
+//            print(data.value(forKey: "loginUsername") as! String)
+//            print(data.value(forKey: "loginPassword") as! String)
+//            print(data.value(forKey: "type") as! Bool)
+//            guard let cat = data.value(forKey: "category") else {
+//                print("Nothing set for category")
+//                continue
+//            }
+//            print(cat)
+//        }
+        
 //        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Users");
 //        request.returnsObjectsAsFaults = false
 //        do {
@@ -130,24 +252,20 @@ class RegisterViewController: UIViewController, UITextFieldDelegate, UIImagePick
         } else {
             registerError.isHidden = true;
             
-            let name = self.name.text;
-            let loginUsername = self.loginUsername.text;
-            let loginPassword = self.loginPassword.text;
-            
             guard let photo = UIImagePNGRepresentation(self.photo.image!) else {
                 print("Failed to convert PNG image")
                 return
             }
             
             //store data in DB
-            storeUserData(name!, loginUsername!, loginPassword!, photo);
+            storeUserData(photo);
 
             //navigate to LoggedIn User Welcome page
             //TODO: call function in LoginController
             let storyBoard: UIStoryboard = UIStoryboard.init(name: "Main", bundle: nil);
             let welcomeController = storyBoard.instantiateViewController(withIdentifier: "welcomeController") as! WelcomeViewController
             
-            welcomeController.userName = name!;
+            welcomeController.userName = self.loginUsername.text!;
             self.present(welcomeController, animated: true, completion: nil)
         }
     }
--- AIY/Controllers/WelcomeViewController.swift
+++ AIY/Controllers/WelcomeViewController.swift
@@ -7,18 +7,26 @@
 //
 
 import UIKit
+import CoreData
 
 class WelcomeViewController: UIViewController {
 
     //MARK: Properties
     @IBOutlet weak var registeredUser: UILabel!
+    @IBOutlet weak var profilePhoto: UIImageView!
     var userName = "";
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        registeredUser.text = userName;
+        let user: Users = getUserInfo(userName)
+//        print(user.name as String?)
+//        print(user.type as Bool?)
+//        print(user.category as String?)
+        let profileImage: UIImage = getUserPhoto(user.photo!)
+        registeredUser.text = user.name
+        profilePhoto.image = profileImage
     }
 
     override func didReceiveMemoryWarning() {
@@ -36,5 +44,39 @@ class WelcomeViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    
+    //fetch user after registration
+    func getUserInfo(_ loginName: String) -> Users {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Users")
+        request.predicate = NSPredicate(format: "loginUsername = %@", loginName)
+        request.returnsObjectsAsFaults = false
+        do {
+            print("Fetching from DB")
+            let result = try context.fetch(request) as! [Users]
+            return result[0]
+        } catch {
+            print("Failed to fetch")
+            let nserror = error as NSError
+            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+        }
+    }
+    
+    //Get user profile picture and resize it to display in 50x50
+    func getUserPhoto(_ photo: NSData) -> UIImage {
+        let pic: UIImage = UIImage(data: photo as Data)!
+        
+        //Resizing logic
+        let newWidth: CGFloat = 50;
+        let scale = newWidth / pic.size.width
+        let newHeight = pic.size.height * scale
+        UIGraphicsBeginImageContext(CGSize(width: newWidth, height: newHeight))
+        pic.draw(in: CGRect(x: 0, y: 0, width: newWidth, height: newHeight))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage!
+    }
 }
